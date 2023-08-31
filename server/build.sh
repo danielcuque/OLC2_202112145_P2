20b7f7691d3af63f552a32bdd1a1ac71e99e83b8
danielcuque <danielcuque78@gmail.com>
2023-08-31 13:41:06 -0600

    chore(server): refactor sh files to run with windows

--- server/build.sh
+++ server/build.sh
@@ -5,5 +5,7 @@ alias grun='java -Xmx500M -cp "~/ANTLR-4.13.0/antlr-4.13.0-complete.jar:$CLASSPA
 
 cd grammar
 
-antlr4 -Dlanguage=Go -o ../chore/parser/ Swift.g4 SwiftLexer.g4 -visitor -no-listener
+-Dlanguage=Go -o ../chore/parser/ Swift.g4 SwiftLexer.g4 -visitor -no-listener
 # antlr4 -Dlanguage=Go -o ../chore/parser/ Swift.g4 -visitor -no-listener
+
+java org.antlr.v4.Tool -Dlanguage=Go -o ../chore/parser/ Swift.g4 SwiftLexer.g4 -visitor -no-listener
