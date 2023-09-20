9abf0c0a284d62f2618d19ddc1c25431bbbdf109
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-09-20 17:35:19 -0600

    feat(server): :art: api response to frontend

--- server/api/api.go
+++ server/api/api.go
@@ -2,86 +2,18 @@ package api
 
 import (
 	I "OLC2/chore/interfaces"
-	U "OLC2/chore/utils"
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-// Get CST image
-func GetCST(input string) string {
-	apiURL := "http://lab.antlr.org/parse/"
-
-	// parse to json lexer, grammar and input
-
-	payload := []byte(
-		fmt.Sprintf(
-			`{
-				"grammar": %s,
-				"input": %s,
-				"lexgrammar": %s,
-				"start": "%s"
-			}`,
-			U.GetParser(),
-			U.FormatInput(input),
-			U.GetLexer(),
-			"program",
-		),
-	)
-
-	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payload))
-
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return ""
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return ""
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading body:", err)
-		return ""
-	}
-
-	// create a map to store the json
-	var data map[string]interface{}
-
-	// // unmarshal the json
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		fmt.Println("Error unmarshalling json:", err)
-		return ""
-	}
-
-	result := data["result"].(map[string]interface{})
-
-	return result["svgtree"].(string)
-
-}
-
 // Response will have symbols, errors, logs and cst
 type Resp struct {
-	Symbols []I.ApiObject     `json:"symbols"`
-	Errors  []*I.VisitorError `json:"errors"`
-	Logs    []string          `json:"logs"`
-	Cst     string            `json:"cst"`
-}
-
-type Message struct {
-	Content string `json:"content"`
+	Errors    []*I.VisitorError `json:"errors"`
+	Symbols   []I.ApiObject     `json:"symbols"`
+	Logs      []string          `json:"logs"`
+	Compiled  string            `json:"compiled"`
+	Optimized string            `json:"optimized"`
 }
 
 // Return errors as json
@@ -92,13 +24,12 @@ func HandleVisitor(c *fiber.Ctx) error {
 
 	result := I.NewEvaluator(code)
 
-	cst := GetCST(code)
-
 	response := Resp{
-		Symbols: result.Env.GetSymbolTable(),
-		Errors:  result.Errors,
-		Logs:    result.Logs,
-		Cst:     cst,
+		Symbols:   result.Env.GetSymbolTable(),
+		Errors:    result.Errors,
+		Logs:      result.Logs,
+		Compiled:  "",
+		Optimized: "",
 	}
 
 	return c.Status(fiber.StatusOK).JSON(response)
