5c17724a7e91f710478557cbbf6f581e466f46d8
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-10-09 22:24:52 -0600

    feat(server): :fire: plus/minus assign for operators

--- server/examples/test.swift
+++ server/examples/test.swift
@@ -1,11 +1,5 @@
 var index = 0
-while index >= 0 {
-    if index == 0 {
-        index = index + 100
-    } else if index > 50 {
-        index = index / 2 - 25
-    } else {
-        index = (index / 2) - 1
-    }
-    print(index)
-}
+index += 10
+print(index)
+index -= 5
+print(index)
