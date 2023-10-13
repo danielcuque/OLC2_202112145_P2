ec54f524cb7c95b82350af558000a4f12400fd60
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-10-12 21:28:34 -0600

    feat(server): :zap: base functions to handle matrix

--- server/core/compiler/for.go
+++ server/core/compiler/for.go
@@ -1,7 +1,11 @@
 package compiler
 
-import "OLC2/core/parser"
+import (
+	"OLC2/core/parser"
+	"fmt"
+)
 
 func (c *Compiler) VisitForStatement(ctx *parser.ForStatementContext) interface{} {
+	fmt.Println("VisitForStatement")
 	return nil
 }
